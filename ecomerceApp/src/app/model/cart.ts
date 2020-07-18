66a3102af38041545ba6f8741a80aa61526a1c68
patukes <osability@gmail.com>
2020-07-18 23:55:34 +0200

    model update

--- ecomerceApp/src/app/model/cart.ts
+++ ecomerceApp/src/app/model/cart.ts
@@ -1,2 +1,4 @@
 export class Cart {
+    cartId: number;
+    products: ProductInOrder[];
 }
---
+++ ecomerceApp/src/app/model/user.ts
@@ -0,0 +1,19 @@
+export class User {
+    email: string;
+
+    password: string;
+
+    name: string;
+
+    phone: string;
+
+    address: string;
+
+    active: boolean;
+
+    role: string;
+
+    constructor(){
+        this.active = true;
+        this.role = Role.Customer;
+}
